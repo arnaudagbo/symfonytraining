a33dd1cd2d474b089e85625fba73a4cfd544dc55
Arnaud AGBO <agbo.arnaud@yahoo.fr>
2018-08-22 17:06:41 +0200

    add article with user

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Controller;
+use App\Entity\Article;
+use App\Entity\User;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+/**
+ * @property EntityManagerInterface em
+ */
+class ArticlesController extends FOSRestController
+{
+    private $articleRepository;
+    public function __construct(ArticleRepository $articleRepository, EntityManagerInterface $em)
+    {
+        $this->articleRepository = $articleRepository;
+        $this->em = $em;
+    }
+    /**
+     * @Rest\View(serializerGroups={"articles"})
+     * @return \FOS\RestBundle\View\View
+     */
+    public function getArticlesAction()
+    {
+        $articles = $this->articleRepository->findAll();
+        return $this->view($articles);
+    }
+    // "get_articles"            [GET] /articles
+    /**
+     * @Rest\View(serializerGroups={"articles"})
+     */
+    public function	getArticleAction($id)
+    {
+        $article = $this->articleRepository->find($id);
+        return $this->view($article);
+    }
+    // "get_article"             [GET] /articles/{id}
+    /**
+     * @Rest\View(serializerGroups={"articles"})
+     * @Rest\Post("/articles")
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     */
+    public function postArticlesAction(Article $article)
+    {
+        // recup user courant
+        $user = $this->getUser();
+        // associe l'user de l'article a l'utilsateu courant
+        $article->setUser($user);
+
+        $this->em->persist($article);
+        $this->em->flush();
+        return $this->view($article);
+    }
+
+    /**
+     * @Rest\View(serializerGroups={"article"})
+     */
+    public function	deleteArtcticleAction($id)
+    {
+        $article = $this->articleRepository->find($id);
+        $this->em->remove($article);
+        $this->em->flush();
+    }
+
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+    /**
+     * @Groups("user")
+     * @Groups("articles")
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $title;
+    /**
+     * @Groups("user")
+     * @Groups("articles")
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $content;
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="articles", cascade={"all"})
+     */
+    private $user;
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+        return $this;
+    }
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+    public function setContent(?string $content): self
+    {
+        $this->content = $content;
+        return $this;
+    }
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+        return $this;
+    }
+}
